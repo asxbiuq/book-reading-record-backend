71268d4ed9ef7985cc978f03a356e4b6ab2fd43a
17o3 <17o3gutk29vinqb@gmail.com>
2022-07-24 22:15:44 +0800

    使用@rollup/plugin-run

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,6 +5,9 @@ import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import run from '@rollup/plugin-run';
+
+const dev = process.env.ROLLUP_WATCH === 'true'
 
 export default defineConfig({
   input: 'src/index.ts',
@@ -13,9 +16,13 @@ export default defineConfig({
     exclude: 'node_modules/**'
   },
   output: {
-    file: 'dist/index.js'
+    file: 'dist/index.js',
+    sourcemap: true
   },
   plugins: [
+    dev && run({
+      execArgv: ['-r', 'source-map-support/register']
+    }),
     // nodeResolve({preferBuiltins: true}),
     // commonjs({
     //   // defaultIsModuleExports:true
